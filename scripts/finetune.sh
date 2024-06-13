bf187db11217e905d5fd9b3390edc7079a9085ae
Agri-LLaVA-Anonymous <todayiskki11@gmail.com>
2024-06-13 13:44:53 +0800

    Add files via upload

---
+++ scripts/finetune.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+deepspeed agri_llava/train/train_mem.py \
+    --deepspeed ./scripts/zero3.json \
+    --model_name_or_path YOUR_MODEL_PATH \
+    --version v1 \
+    --data_path YOUR_DATA_PATH \
+    --image_folder YOUR_IMG_PATH \
+    --vision_tower openai/clip-vit-large-patch14-336 \
+    --mm_projector_type mlp2x_gelu \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end False \
+    --mm_use_im_patch_token False \
+    --image_aspect_ratio pad \
+    --group_by_modality_length True \
+    --bf16 True \
+    --output_dir YOUR_MODEL_SAVE_PATH \
+    --num_train_epochs 3 \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 50000 \
+    --save_total_limit 3 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --dataloader_num_workers 4 \
+    --lazy_preprocess True \
+    --report_to wandb
